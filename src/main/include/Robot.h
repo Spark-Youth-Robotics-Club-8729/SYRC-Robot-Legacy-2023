987940ffa9696319468638a9fae46a086e222e57
Karan Chawla <87392846+KaranChawla11@users.noreply.github.com>
2022-01-19 19:51:43 -0500

    Add files via upload

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -13,6 +13,12 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc/Encoder.h>
 #include <frc/simulation/EncoderSim.h>
+#include <frc/AnalogInput.h>
+#include <frc/DigitalOutput.h>
+#include "frc/RobotController.h"
+#include "rev/ColorSensorV3.h"
+#include "rev/ColorMatch.h"
+#include <frc/util/color.h>
 
 
 class Robot : public frc::TimedRobot {
@@ -44,6 +50,19 @@ private:
   static const int EncoderPin2A = 2;
   static const int EncoderPin2B = 3;
 
+  //I2C Port
+  static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
+
+  //Colour Sensor
+  rev::ColorSensorV3 m_colorSensor{i2cPort};
+  rev::ColorMatch m_colorMatcher;
+
+  //RGB Values
+  static constexpr frc::Color kBlueCargo = frc::Color(0, 0, 0);
+  static constexpr frc::Color kRedCargo = frc::Color(0, 0, 0);
+  static constexpr frc::Color kBlueTarmac = frc::Color(0, 0, 0);
+  static constexpr frc::Color kBlackLine = frc::Color(0, 0, 0); //We'll need to detect these on Sunday
+
   bool arrivedDestination = false;
 
   //Joystick
@@ -60,7 +79,12 @@ private:
   frc::Encoder m_encoder1{ EncoderPin1A, EncoderPin1B, true };
   frc::Encoder m_encoder2{ EncoderPin2A, EncoderPin2B, false };
 
- 
+  // Ultrasonic Set Up
+  frc::AnalogInput ultrasonic_sensor_one{0};
+  frc::DigitalOutput ultrasonic_trigger_pin_one{0};
+  double ultrasonic_sensor_range_one = 0;
+  double voltage_scale_factor = 1;
+
   //Default
   frc::SendableChooser<std::string> m_chooser;
   const std::string kAutoNameDefault = "Default";
