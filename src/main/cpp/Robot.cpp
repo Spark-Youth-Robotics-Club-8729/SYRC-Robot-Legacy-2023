8bee4172656d00111b870a7a78751ce210dedf8f
Karan Chawla <87392846+KaranChawla11@users.noreply.github.com>
2022-01-27 22:32:01 -0500

    Add files via upload

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,4 +1,4 @@
-// XD Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -49,9 +49,9 @@ void Robot::RobotPeriodic() {
 
 
   double voltage_scale_factor = 5/frc::RobotController::GetVoltage5V();
-  double  ultrasonic_sensor_range_one = ultrasonic_sensor_one.GetValue() * 0.125 * voltage_scale_factor;
+  double  ultrasonic_sensor_range_one = ultrasonic_sensor_one.GetValue() * 0.0492 * voltage_scale_factor;
+  double  ultrasonic_sensor_range_two = ultrasonic_sensor_one.GetValue() * 0.0492 * voltage_scale_factor;
   frc::SmartDashboard::PutNumber("Sensor 1 Range", ultrasonic_sensor_range_one);
-
 
 }
 
@@ -61,10 +61,20 @@ void Robot::AutonomousInit()
 m_encoder1.Reset();
 m_encoder2.Reset();
 ultrasonic_trigger_pin_one.Set(true);
-distance = true;
-once = true;
+// inRange = true;
+// once = true;
+// resetOnce=false;
+// encodersReset=false;
+// outtakeBallSecondTime=false;
 pastBlue = 0.0;
 currentBlue = 0.0;
+distance = true;
+cargo_Outtake_Time = 0;
+phase=0;
+phase4=0;
+encoderAverage=0.0;
+reset=false;
+closetoCargo = false;
 
 }
 
@@ -77,22 +87,12 @@ frc::SmartDashboard::PutNumber("Green", detectedColor.green);
 frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
 pastBlue = currentBlue;
 currentBlue = detectedColor.blue;
-int distanceFromTarmac = 0;
+int distanceFromTarmac = 0; 
 
-m_robotDrive.ArcadeDrive(1,0);
-  frc::Color detectedColor = m_colorSensor.GetColor();  
-  std::string colorString;
-  double confidence = 0.0;
-  frc::Color matchedColor = m_colorMatcher.MatchClosestColor(detectedColor, confidence); //Detecting the colour our sensor sees
-
-  // Ultrasonic
-  //double  ultrasonic_sensor_range_one = ultrasonic_sensor_one.GetValue() * 0.0492 * voltage_scale_factor;
-  // if (ultrasonic_sensor_range_one > 10) {
-  //   m_robotDrive.ArcadeDrive(1, 0);
-  // } else {
-  //   m_robotDrive.ArcadeDrive(0, 0.5);
-  // }
-  
+encoderAverage = (m_encoder1.GetDistance() + m_encoder2.GetDistance())/2;
+// std::string colorString;
+// double confidence = 0.0;
+// frc::Color matchedColor = m_colorMatcher.MatchClosestColor(detectedColor, confidence); //Detecting the colour our sensor sees
 
   // Route 3
   // if (ultrasonic_sensor_range_one>5) { 
@@ -101,12 +101,9 @@ m_robotDrive.ArcadeDrive(1,0);
   //   m_robotDrive.ArcadeDrive(0, 0);
   //   // add motor to shoot
   // }
-  while (matchedColor == kBlueTarmac) {
-    m_robotDrive.ArcadeDrive(0.8, 0);
-  }
-
-
-  
+  // while (matchedColor == kBlueTarmac) {
+  //   m_robotDrive.ArcadeDrive(0.8, 0);
+  // }
 
 
 // Route 1: 
@@ -123,101 +120,284 @@ m_robotDrive.ArcadeDrive(1,0);
 // 5. Outtake into lower hub
 // 6. Drive forward to get out of tarmac
   
-  //This is code assuming we DON'T start next to the hub. Will keep for testing purposes if you delete !kick
-  if (ultrasonic_sensor_range_one > 12 && distance == true) { //Change 5 to set distance //Start of 1. and 4. Checking if ultrasonic in range
-    m_robotDrive.ArcadeDrive(-0.75, 0);
-  } else 
-  {
-    distance = false;
-    m_encoder1.Reset();
-    m_encoder2.Reset();
-    if ((m_encoder1.GetDistance() + m_encoder2.GetDistance())/2 > -6) //Ultrasonic can't be too precise, so distance needs to go rest of remaining distance
-    {
-      m_robotDrive.ArcadeDrive (-0.75, 0); 
-    } 
-    else 
-    {
-      //Shoot/Drop ball here
-    }
+  // //This is code assuming we DON'T start next to the hub. Will keep for testing purposes if you delete !kick
+  // if (ultrasonic_sensor_range_one >= 12 && inRange == true) 
+  // { //Change 5 to set distance //Start of 1. and 4. Checking if ultrasonic in range
+  //   m_robotDrive.ArcadeDrive(-0.75, 0);
+  // } 
+  // else if (encodersReset==false)
+  // { 
+  //   encodersReset = true;
+  //   inRange = false;
+  //   if (resetOnce==false)
+  //   {
+  //     resetOnce=true;
+  //     m_encoder1.Reset();
+  //     m_encoder2.Reset();
+  //   }
+  //   if ((m_encoder1.GetDistance() + m_encoder2.GetDistance())/2 > 6) //Ultrasonic can't be too precise, so encoder needs to check rest of remaining distance
+  //   {
+  //     m_robotDrive.ArcadeDrive (-0.75, 0); 
+  //   } 
+  //   else 
+  //   {
+  //     //Shoot/Drop ball here (Outtake Code)
+  //     m_encoder1.Reset(); 
+  //     m_encoder2.Reset();
+  //   }
+  // } 
+  // else if (outtakeBallSecondTime==true)
+  // {
+  //   if (currentBlue < pastBlue + 150)
+  //   {
+  //     m_robotDrive.ArcadeDrive (0.75, 0);
+  //   }
+  //   else
+  //   {
+  //   outtakeBallSecondTime=false;
+  //   }
+  // }
+  // if (currentBlue < pastBlue + 150 && once == true && inRange==false) //Start of 2. //Need to test "150" number. TO CLARIFY: Current blue is the current color sensed, and past blue is the one sensed 20ms
+  //                                   // ago (iteration period). +150 is colour not distance
+  // {
+  //   m_robotDrive.ArcadeDrive(0.75, 0);
+  // } //Below we would align robot if we are doing that (maybe)
+  // else if (inRange==false && once==true)
+  // {
+  //   once=false;
+  // }
+  // else if (m_encoder1.GetDistance() < 350/8 && once==false) //Distance is 323/8 -- Should be less/more (test) than this number. This if is to go to cargo ball.                                                       //Only enters once colour sensor out of tarmac. Only enters once bc we only pick up ball once :glasses:                                
+  //   {
+  //   m_robotDrive.ArcadeDrive(0.5, 0);
+  //     if (m_encoder1.GetDistance() > 275/8) //You are bit before cargo, start intake
+  //     {
+  //       //Turn on Intake to get ball in robot :skull:
+  //     }
+  //   //End of 2.   // add intake motor to intake
+  //   } else if(m_encoder1.GetDistance() >= 350/8){
+  //       inRange = true; 
+  //       resetOnce = false;
+  //       once=true;
+  //       outtakeBallSecondTime=true;
+  //   }
+
+  // Phase 0: Drive from starting position to hub
+if (phase == 0) { 
+  if (ultrasonic_sensor_range_one > 12 && distance == true) { // Checking if ultrasonic is in range of minimum 12 inches away from hub
+    m_robotDrive.ArcadeDrive(-0.75, 0); // Drive towards hub
   } 
-  if (currentBlue < pastBlue + 150) //Start of 2. //Need to test "150" number. TO CLARIFY: Current blue is the current color sensed, and past blue is the one sensed 20ms
-                                    // ago (iteration period). +150 is colour not distance
-  {
-    m_robotDrive.ArcadeDrive(0.75, 0); //Below we would allign robot if we are doing that (maybe)
-    m_encoder1.Reset();
-  } 
-  else 
-  {
-    if (m_encoder1.GetDistance() < 350/8 && once == true) //Distance is 323/8 -- Should be less/more (test) than this number. This if is to go to cargo ball. 
-                                                          //Only enters once colour sensor out of tarmac. Only enters once bc we only pick up ball once :glasses:
-                                                        
-    {
-      if (m_encoder1.GetDistance() > 275/8) //You are at cargo pick it up bozo
-      {
-        //Turn on Intake to get ball in robot :skull:
-      }
-    m_robotDrive.ArcadeDrive(0.5, 0);
-    //End of 2.   // add intake motor to intake
-      distance = true; 
-      once = false;
-    } 
-  }
-
-  
-
-  
-
-
-  // if (matchedColor == kBlueTarmac) {}  //Example Colour Sensor if statement
-
-  
-//   if (m_encoder1.GetDistance() < 10 && arrivedDestination == false) {
-//     m_robotDrive.ArcadeDrive(0, 0.5);
-//     } else { 
-//         arrivedDestination = true;
-//         if(m_encoder1.GetDistance() > 0) {
-//             m_robotDrive.ArcadeDrive(0, -0.5);
-//         }
-//         else {
-            // m_robotDrive.ArcadeDrive(0, 0); //Take in values for ArcadeDrive(turning, -(going forward))
-//         } 
-//     }
-
-
-    //ROUTE 2
-    double halfFromHubToCargo = 123.25;
-    int angleTurn=0; //To be determined
-  if (ultrasonic_sensor_range_one > 12 && distance == true) { //Change 5 to set distance //Start of 1. and 4.
-    m_robotDrive.ArcadeDrive(-0.75, 0);
-    //Shoot ball
-  } else
-  {
+  else { // Ultrasonic is too close to be precise (12 or less away from hub)
     distance = false;
-    if (m_encoder1.GetDistance()< 5){
-      m_robotDrive.ArcadeDrive(-0.5, 0);
+    if (encoderAverage > -6) { // Use encoders to measure distance now
+      m_robotDrive.ArcadeDrive(-0.75, 0); // Drive until 6 inches from hub
     } 
-  }
-  if (matchedColor == kBlueTarmac) //Start of 2.
-  {
-    m_robotDrive.ArcadeDrive(0.75, 0);
-    m_encoder1.Reset();
-  } else if (m_encoder1.GetDistance() < 360/8 && once == true) //Should be less than this number
-  {
-    m_robotDrive.ArcadeDrive(0.5, 0);
-    if (m_encoder1.GetDistance() >275/8){
-      printf("Run Intake");
-      // Intake code
+    else { // Arrived at desired distance from hub for outtake
+      m_robotDrive.ArcadeDrive(0, 0);
+      phase = 1;
     }
-  //End of 2.   // add intake motor to intake
-    distance = true; 
-    once = false;
-  } else if (ultrasonic_sensor_range_one > 5)
-  {
-    m_robotDrive.ArcadeDrive (0.75, 0);
   }
-  
 }
 
+// Phase 1: Outtake ball
+if (phase == 1) {
+  if (cargo_Outtake_Time < 50) { // 50 multiplied by 20 millisecond period is approximately 1 second
+    // Outtake on
+    cargo_Outtake_Time++; 
+  }
+  else { // Done dropping cargo into hub
+    // Outtake off
+    phase = 2;
+    cargo_Outtake_Time = 0;
+    cargo_Intake_Time = 0;
+  }
+}
+
+// Phase 2.5: Drive until outside the tarmac
+if (phase == 2 && reset == true) {
+  if (ultrasonic_sensor_range_one > 6) {  
+    m_robotDrive.ArcadeDrive(0.75, 0);
+  }
+}
+
+// Phase 2: Drive to edge of tarmac
+if (phase == 2 && reset == false) {
+  if (currentBlue < pastBlue + 150) {  // If current blue value is within 150 (this range can be tested) of past blue value, we are assuming that the surface being sensed has not changed
+    m_robotDrive.ArcadeDrive(0.75, 0); // Continue driving until tarmac outline reached; below we would align robot if we are doing that (maybe)
+  } 
+  else { // Tarmac outline reached (blue tape sensed) 
+    phase = 3;
+    m_encoder1.Reset();
+    m_encoder2.Reset(); // Prepare for phase 3
+  }
+} 
+
+// Phase 3: Drive to cargo
+if (phase == 3) {
+  if (encoderAverage < 350/8 && closetoCargo==false) { // Real distance = 323/8 inches
+    m_robotDrive.ArcadeDrive(0.75, 0);
+    if (encoderAverage < 270/8) {
+      closetoCargo=true;
+    }
+  }
+  if (cargo_Intake_Time < 150) { 
+    // Intake on
+    cargo_Intake_Time++; 
+    m_robotDrive.ArcadeDrive(0.75,0);
+  }
+  else { // Done picking up cargo
+    // Intake off
+    phase = 0;
+    distance = true;
+    reset = true;
+    cargo_Intake_Time = 0;
+    m_encoder1.Reset();
+    m_encoder2.Reset(); // Back to phase 0
+  }
+}
+// //Phase 5: Drive back to hub
+
+//Things to do today:
+// Turn on intake while driving from the tarmac to the cargo ball
+
+//   //DARUN GROUP
+// bool darun = true; 
+//   //This is code assuming we DON'T start next to the hub. Will keep for testing purposes if you delete !kick
+//   if (ultrasonic_sensor_range_one >= 12 && inRange == true) 
+//   { //Change 5 to set distance //Start of 1. and 4. Checking if ultrasonic in range
+//     m_robotDrive.ArcadeDrive(-0.75, 0);
+//   } 
+//   else if (darun)
+//   {
+//     inRange = false;
+//     if (resetOnce==false) //&& variable = 1){
+//       resetOnce=true;
+//       m_encoder1.Reset();
+//       m_encoder2.Reset();
+//       //variable = 2
+//     }
+//     if ((m_encoder1.GetDistance() + m_encoder2.GetDistance())/2 > 6) //Ultrasonic can't be too precise, so encoder needs to check rest of remaining distance
+//     {
+//       m_robotDrive.ArcadeDrive (-0.75, 0); 
+//     } 
+//     else 
+//     {
+//       //Shoot/Drop ball here (Outtake Code)
+//       darun = false;
+//     }
+                                                           
+// int Darun{}; //bruh use proper variable names :skull: y is there and erro alr i think we're technically set??
+// if (Darun != 1) { 
+//   if (currentBlue < pastBlue + 150 && once == true && inRange==false) //Start of 2. //Need to test "150" number. TO CLARIFY: Current blue is the current color sensed, and past blue is the one sensed 20ms ago (iteration period). +150 is colour not distance
+//   {
+//     m_robotDrive.ArcadeDrive(0.75, 0);
+//   } //Below we would align robot if we are doing that (maybe)
+  
+//   else if (inRange==false && once==true)
+//   {
+//     once=false;
+//     m_encoder1.Reset(); 
+//   }
+  
+//   else if (m_encoder1.GetDistance() < 350/8 && once==false) //Distance is 323/8 -- Should be less/more (test) than this number. This if is to go to cargo ball.                                                       //Only enters once colour sensor out of tarmac. Only enters once bc we only pick up ball once :glasses:                                
+//   {
+//     m_robotDrive.ArcadeDrive(0.5, 0);
+//       if (m_encoder1.GetDistance() > 275/8) //You are bit before cargo, start intake
+//       {
+//         //Turn on Intake to get ball in robot 💀
+//         Darun ++;
+//       }
+//     //End of 2.   // add intake motor to intake
+     
+//       else if(m_encoder1.GetDistance() >= 350/8)
+//       {
+//           inRange = true; 
+//           resetOnce = false;
+//           once=true;
+//       }
+//   }
+//   //HINT: Just boolean spam, also don't go back randomly you need to STOP at a certain point cough cough ultrasonic sensor. Unmute if you think you have the solution
+// }
+// else
+// {
+//   if(ultrasonic_sensor_range_two() < 12){
+//     m_robotDrive.ArcadeDrive(-0.75, 0);
+  // }
+
+ // Route 4
+ int phase3=0;
+ //Phase 0: Drop off ball at terminal
+ if (phase3 == 0) { 
+  if (cargo_Outtake_Time < 50){
+    cargo_Outtake_Time++;
+  }
+  else {
+    phase3 = 2;
+    cargo_Outtake_Time = 0;
+    cargo_Intake_Time = 0;
+  }
+ }
+
+ //Phase two: Back up untill you are 34.2 inches away from wall. 
+ if(ultrasonic_sensor_range_one>34.2){
+   m_robotDrive.ArcadeDrive(0.75,0);
+ }
+ //Phase three: use nav sensor to turn 90 degrees
+ 
+ 
+ 
+
+// Route 3
+
+// Phase 0: Drive from starting position to hub
+if (phase4 == 0) { 
+  if (ultrasonic_sensor_range_one > 12 && distance == true) { // Checking if ultrasonic is in range of minimum 12 inches away from hub
+    m_robotDrive.ArcadeDrive(-0.75, 0); // Drive towards hub
+  } 
+  else { // Ultrasonic is too close to be precise (12 or less away from hub)
+    distance = false;
+    if (encoderAverage > -6) { // Use encoders to measure distance now
+      m_robotDrive.ArcadeDrive(-0.75, 0); // Drive until 6 inches from hub
+    } 
+    else { // Arrived at desired distance from hub for outtake
+      m_robotDrive.ArcadeDrive(0, 0);
+      phase4 = 1;
+    }
+  }
+}
+
+// Phase 1: Outtake ball
+if (phase4 == 1) {
+  if (cargo_Outtake_Time < 50) { // 50 multiplied by 20 millisecond period is approximately 1 second
+    // Outtake on
+    cargo_Outtake_Time++; 
+  }
+  else { // Done dropping cargo into hub
+    // Outtake off
+    phase4 = 2;
+    cargo_Outtake_Time = 0;
+    cargo_Intake_Time = 0;
+  }
+}
+
+if (phase == 2 && reset == false) {
+  if (currentBlue < pastBlue + 150) {  // If current blue value is within 150 (this range can be tested) of past blue value, we are assuming that the surface being sensed has not changed
+    m_robotDrive.ArcadeDrive(0.75, 0); // Continue driving until tarmac outline reached; below we would align robot if we are doing that (maybe)
+  } 
+  else { // Tarmac outline reached (blue tape sensed) 
+    phase = 3;
+    m_encoder1.Reset();
+    m_encoder2.Reset(); // Prepare for phase 3
+  }
+} 
+
+// Phase 3
+if (phase == 3){
+  if (encoderAverage < 2){
+    
+  }
+}
+
+} 
 void Robot::TeleopInit() {
   m_encoder1.Reset();
   m_encoder2.Reset();
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -11,6 +11,7 @@
 #include <frc/drive/DifferentialDrive.h>
 #include "rev/CANSparkMax.h"
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/smartdashboard/Field2d.h>
 #include <frc/Encoder.h>
 #include <frc/simulation/EncoderSim.h>
 #include <frc/AnalogInput.h>
@@ -19,6 +20,10 @@
 #include "rev/ColorSensorV3.h"
 #include "rev/ColorMatch.h"
 #include <frc/util/color.h>
+#include <frc/AnalogGyro.h>
+#include <frc/kinematics/DifferentialDriveOdometry.h>
+
+
 
 
 class Robot : public frc::TimedRobot {
@@ -50,20 +55,28 @@ private:
   static const int EncoderPin2A = 2;
   static const int EncoderPin2B = 3;
 
-  //I2C Port
+  // Field2d
+  // frc::Field2d m_field;
+
+  // I2C Port
   static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
 
-  //Colour Sensor
+  // Colour Sensor
   rev::ColorSensorV3 m_colorSensor{i2cPort};
   rev::ColorMatch m_colorMatcher;
+  float currentBlue;
+  float pastBlue;
 
-  //RGB Values
+  //Gryo
+  // frc::AnalogGyro m_gyro{0};
+
+  // RGB Values
   static constexpr frc::Color kBlueCargo = frc::Color(0, 0, 0);
   static constexpr frc::Color kRedCargo = frc::Color(0, 0, 0);
   static constexpr frc::Color kBlueTarmac = frc::Color(0, 0, 0);
   static constexpr frc::Color kBlackLine = frc::Color(0, 0, 0); //We'll need to detect these on Sunday
 
-  bool arrivedDestination = false;
+  // bool arrivedDestination = false;
 
   //Joystick
   frc::Joystick m_xbox{ 0 };
@@ -78,12 +91,24 @@ private:
   //Encoder Set Up
   frc::Encoder m_encoder1{ EncoderPin1A, EncoderPin1B, true };
   frc::Encoder m_encoder2{ EncoderPin2A, EncoderPin2B, false };
+  float encoderAverage;
 
   // Ultrasonic Set Up
   frc::AnalogInput ultrasonic_sensor_one{0};
-  frc::DigitalOutput ultrasonic_trigger_pin_one{0};
+  frc::DigitalOutput ultrasonic_trigger_pin_one{4};
   double ultrasonic_sensor_range_one = 0.0;
   double voltage_scale_factor = 1.0;
+
+  // Autonomous Set Up
+  int counter;
+  int phase;
+  int phase4;
+  int cargo_Outtake_Time;
+  int cargo_Intake_Time;
+  bool distance;
+  bool reset;
+  bool closetoCargo;
+  // bool once;
 
   //Default
   frc::SendableChooser<std::string> m_chooser;
