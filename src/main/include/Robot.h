ed1c8fe4cb22d280249f447f2d55d1d1081ba762
Iwillgetmy <74886842+Iwillgetmy@users.noreply.github.com>
2022-02-11 17:59:47 -0500

    Update Robot.h

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -1,139 +1,198 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+  // Copyright (c) FIRST and other WPILib contributors.
+  // Open Source Software; you can modify and/or share it under the terms of
+  // the WPILib BSD license file in the root directory of this project.
 
-#pragma once
+  #include "Robot.h"
 
-#include <string>
-#include <frc/Joystick.h>
-#include <frc/TimedRobot.h>
-#include <frc/smartdashboard/SendableChooser.h>
-#include <frc/drive/DifferentialDrive.h>
-#include "rev/CANSparkMax.h"
-#include <frc/smartdashboard/SmartDashboard.h>
-#include <frc/smartdashboard/Field2d.h>
-#include <frc/Encoder.h>
-#include <frc/simulation/EncoderSim.h>
-#include <frc/AnalogInput.h>
-#include <frc/DigitalOutput.h>
-#include "frc/RobotController.h"
-#include "rev/ColorSensorV3.h"
-#include "rev/ColorMatch.h"
-#include <frc/util/color.h>
-#include <frc/AnalogGyro.h>
-#include <frc/kinematics/DifferentialDriveOdometry.h>
-#include "cameraserver/CameraServer.h"
-#include "frc/motorcontrol/PWMVictorSPX.h"
-#include <frc/SpeedControllerGroup.h>
+  #include <fmt/core.h>
 
+  #include <frc/smartdashboard/SmartDashboard.h>
 
+  #include "rev/ColorSensorV3.h"
 
+  void Robot::RobotInit() {
 
+    //Reset Motors
+    m_leftLeadMotor.RestoreFactoryDefaults();
+    m_rightLeadMotor.RestoreFactoryDefaults();
+    m_leftFollowMotor.RestoreFactoryDefaults();
+    m_rightFollowMotor.RestoreFactoryDefaults();
 
-class Robot : public frc::TimedRobot {
+    //Allowing back motors to follow front motors
+    m_leftFollowMotor.Follow(m_leftLeadMotor);
+    m_rightFollowMotor.Follow(m_rightLeadMotor);
 
-public:
+    //Default
+    m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+    m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+    frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 
-  void RobotInit() override;
-  void RobotPeriodic() override;
-  void AutonomousInit() override;
-  void AutonomousPeriodic() override;
-  void TeleopInit() override;
-  void TeleopPeriodic() override;
-  void DisabledInit() override;
-  void DisabledPeriodic() override;
-  void TestInit() override;
-  void TestPeriodic() override;
+    //Camera
+    cs::UsbCamera camera = frc::CameraServer::StartAutomaticCapture();
+    camera.SetResolution (640, 480);
+    
+    //Encoders
+    m_encoder1.Reset();
+    m_encoder2.Reset();
 
-private:
+    m_encoder1.SetDistancePerPulse((3.14159265358 * 6) / 360.0);
+    m_encoder2.SetDistancePerPulse((3.14159265358 * 6) / 360.0);
 
-  //CAN Pin Constants
-  static const int leftLeadDeviceID = 2;
-  static const int leftFollowDeviceID = 3;
-  static const int rightLeadDeviceID = 1;
-  static const int rightFollowDeviceID = 4;
-  static const int Shooter1RioPin = 1;
-  static const int Shooter2RioPin = 2;
-  static const int IntakeMotorRioPin = 3;
+    // Ultrasonic 
+    ultrasonic_trigger_pin_one.Set(true);
 
+    //Colour Matcher
+    // m_colorMatcher.AddColorMatch(kBlueCargo);
+    // m_colorMatcher.AddColorMatch(kRedCargo);
+    // m_colorMatcher.AddColorMatch(kBlackLine); 
+    // m_colorMatcher.AddColorMatch(kBlueTarmac);
 
+  }
 
-  //Encoder Pin Constants
-  static const int EncoderPin1A = 0;
-  static const int EncoderPin1B = 1;
-  static const int EncoderPin2A = 2;
-  static const int EncoderPin2B = 3;
+  void Robot::RobotPeriodic() {
 
-  // Field2d
-  // frc::Field2d m_field;
+  }
 
-  // I2C Port
-  static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
+  void Robot::AutonomousInit() 
+  {
+  //Reset/Initializing Sensors
+  m_encoder1.Reset();
+  m_encoder2.Reset();
+  ultrasonic_trigger_pin_one.Set(true);
 
-  // Colour Sensor
-  rev::ColorSensorV3 m_colorSensor{i2cPort};
-  rev::ColorMatch m_colorMatcher;
-  float currentRed;
-  float pastRed;
+  //Variables (bools, phases)
 
-  
+  // inRange = true;
+  // once = true;
+  // resetOnce=false;
+  // encodersReset=false;
+  // outtakeBallSecondTime=false;
+  pastRed = 0.0;
+  currentRed = 0.269;
+  distance = true;
+  cargo_Outtake_Time = 0;
+  phase=0;
+  phase4=0;
+  encoderAverage=0.0;
+  reset=false;
+  closetoCargo = false;
+  increment = 0;
+  SenseColour = false;
+  }
 
-  // RGB Values
-  // static constexpr frc::Color kBlueCargo = frc::Color(0, 0, 0);
-  // static constexpr frc::Color kRedCargo = frc::Color(0, 0, 0);
-  // static constexpr frc::Color kBlueTarmac = frc::Color(0, 0, 0);
-  // static constexpr frc::Color kBlackLine = frc::Color(0, 0, 0); //We'll need to detect these on Sunday
+  void Robot::AutonomousPeriodic() 
+  {
 
-  // bool arrivedDestination = false;
+  //Colour Sensor Periodic
+  frc::Color detectedColor = m_colorSensor.GetColor();  
+  frc::SmartDashboard::PutNumber("Red", detectedColor.red);
+  frc::SmartDashboard::PutNumber("Green", detectedColor.green);
+  frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
+  currentRed = detectedColor.red;
 
-  //Joystick
-  frc::Joystick m_xbox{ 0 };
+  //Ultrasonic Sensor Periodic
+  double voltage_scale_factor = 5/frc::RobotController::GetVoltage5V();
+  double  ultrasonic_sensor_range_one = ultrasonic_sensor_one.GetValue() * 0.0492 * voltage_scale_factor; //How far ultrasonic is from viewable object 
+  frc::SmartDashboard::PutNumber("Sensor 1 Range", ultrasonic_sensor_range_one);
 
-  //Drive
-  rev::CANSparkMax m_leftLeadMotor{leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushed};
-  rev::CANSparkMax m_rightLeadMotor{rightLeadDeviceID, rev::CANSparkMax::MotorType::kBrushed};
-  rev::CANSparkMax m_leftFollowMotor{leftFollowDeviceID, rev::CANSparkMax::MotorType::kBrushed};
-  rev::CANSparkMax m_rightFollowMotor{rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushed};
-  frc::DifferentialDrive m_robotDrive{m_leftLeadMotor, m_rightLeadMotor};
+  //Encoder Periodic
+  encoderAverage = (m_encoder1.GetDistance() + m_encoder2.GetDistance())/2; //Average of ultrasonic 1 and 2 reading
 
-  //Intake/Shooter
-  frc::PWMVictorSPX m_Shooter1Motor {Shooter1RioPin};
-  frc::PWMVictorSPX m_Shooter2Motor {Shooter2RioPin};
-  frc::PWMVictorSPX m_IntakeMotor {IntakeMotorRioPin};
-  frc::SpeedControllerGroup m_Shooter {m_Shooter1Motor, m_Shooter2Motor};
+  //********************************************************************************************************************************
 
-  //Encoder Set Up
-  frc::Encoder m_encoder1{ EncoderPin1A, EncoderPin1B, true };
-  frc::Encoder m_encoder2{ EncoderPin2A, EncoderPin2B, false };
-  float encoderAverage;
+    
+    if (phase4==0){ //Turn 180 degrees
+      if(m_encoder1.GetDistance()>=-40.12){
+        m_robotDrive.ArcadeDrive(0.6,0);
+      }else{
+        phase4=1;
+        m_encoder1.Reset();
+        m_encoder2.Reset();
+      }
+    }
+    if (phase4==1) //Go back until 40 inches away from wall
+    {
+      if (ultrasonic_sensor_range_one>=20.0){
+        m_robotDrive.ArcadeDrive(0, 0.5); // Drive until 40 inches from back wall
+        m_encoder1.Reset();
+        m_encoder2.Reset();
+      }else{
+        phase4=2;
+      }
+    }
+  if (phase4==2){ //Turn 90 degrees
+      if(m_encoder2.GetDistance()<20.06){
+        m_robotDrive.ArcadeDrive(0.5,0);
+      }else{
+        phase4=3;
+        m_encoder1.Reset();
+        m_encoder2.Reset();
+      }
+    }
+    //Go fowards till terminal ball and push it to the human player
+    if (phase4==3){
+      if(m_encoder2.GetDistance()<20){
+        m_robotDrive.ArcadeDrive(0,0.4);
+      } else{
+        if (cargo_Intake_Time < 150) { 
+          // Intake on
+          cargo_Intake_Time++; 
+          m_robotDrive.ArcadeDrive(0, 0.5);
+          phase4=4;
+        }
+      }
+    //That is it???
+    }
+  } 
 
-  // Ultrasonic Set Up
-  frc::AnalogInput ultrasonic_sensor_one{0};
-  frc::DigitalOutput ultrasonic_trigger_pin_one{4};
-  double ultrasonic_sensor_range_one = 0.0;
-  
-  // frc::AnalogInput ultrasonic_sensor_two{0};
-  // frc::DigitalOutput ultrasonic_trigger_pin_two{4};
-  // double ultrasonic_sensor_range_two = 0.0;
+  void Robot::TeleopInit() {
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+  }
 
-  double voltage_scale_factor = 1.0;
+  void Robot::TeleopPeriodic() {
 
-  // Autonomous Set Up
-  int counter;
-  int phase;
-  int phase4;
-  int cargo_Outtake_Time;
-  int cargo_Intake_Time;
-  bool distance;
-  bool reset;
-  bool closetoCargo;
-  int increment;
-  bool SenseColour;
-  // bool once;
+    frc::Color detectedColor = m_colorSensor.GetColor();  
+    frc::SmartDashboard::PutNumber("Red", detectedColor.red);
+    frc::SmartDashboard::PutNumber("Green", detectedColor.green);
+    frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
 
-  //Default
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
-};
+    // Drive with arcade style
+    float xDrive = m_xbox.GetRawAxis(0) * -0.8;
+    float yDrive = m_xbox.GetRawAxis(1) * 0.8;
+    m_robotDrive.ArcadeDrive(xDrive, yDrive);
+    m_IntakeMotor.Set(m_xbox.GetRawAxis(3));
+    m_Shooter.Set(m_xbox.GetRawAxis(1) * -1.0);
+
+    double voltage_scale_factor = 5/frc::RobotController::GetVoltage5V();
+    double  ultrasonic_sensor_range_one = ultrasonic_sensor_one.GetValue() * 0.0492 * voltage_scale_factor;
+    double  ultrasonic_sensor_range_two = ultrasonic_sensor_one.GetValue() * 0.0492 * voltage_scale_factor;
+    frc::SmartDashboard::PutNumber("Sensor 1 Range", ultrasonic_sensor_range_one);
+    frc::SmartDashboard::PutNumber("Sensor 2 Range", ultrasonic_sensor_range_two);
+
+    // frc::DifferentialDriveOdometry m_odometry{m_gyro.GetRotation2d()};
+    // m_odometry.Update(m_gyro.GetRotation2d(),
+    //                   units::meter_t(m_encoder1.GetDistance()),  
+    //                   units::meter_t(m_encoder2.GetDistance()));
+    // m_field.SetRobotPose(m_odometry.GetPose());
+    // Encoder SmartDashboard
+    frc::SmartDashboard::PutNumber("Encoder 1 Distance: ", m_encoder1.GetDistance());
+    frc::SmartDashboard::PutNumber("Encoder 2 Distance: ", m_encoder2.GetDistance());
+    frc::SmartDashboard::PutNumber("Robot Displacement: ", (m_encoder1.GetDistance() + m_encoder2.GetDistance())/2);
+    // frc::SmartDashboard::PutData("Field", &m_field);
+  }
+
+  void Robot::DisabledInit() {}
+
+  void Robot::DisabledPeriodic() {}
+
+  void Robot::TestInit() {}
+
+  void Robot::TestPeriodic() {}
+
+  #ifndef RUNNING_FRC_TESTS
+  int main() {
+
+    return frc::StartRobot<Robot>();
+  }
+  #endif
