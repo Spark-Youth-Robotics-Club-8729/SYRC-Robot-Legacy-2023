f241b8d50fb6a6edd5e19b86554f668ead465c13
Karan Chawla <87392846+KaranChawlaD@users.noreply.github.com>
2022-02-21 17:30:27 -0500

    Add files via upload

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -2,36 +2,22 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+//Libraries
 #include "Robot.h"
 #include <stdlib.h>
 #include <fmt/core.h>
 
-#include <frc/smartdashboard/SmartDashboard.h>
-
-#include "rev/ColorSensorV3.h"
-
 void Robot::RobotInit() {
 
-  //Reset Motors
-  m_leftLeadMotor.RestoreFactoryDefaults();
-  m_rightLeadMotor.RestoreFactoryDefaults();
-  m_leftFollowMotor.RestoreFactoryDefaults();
-  m_rightFollowMotor.RestoreFactoryDefaults();
+  //Reset Shooter and Storage motors
   m_shooter1.RestoreFactoryDefaults();
   m_shooter2.RestoreFactoryDefaults();
   m_storage.RestoreFactoryDefaults();
 
 
-  //Allowing back motors to follow front motors
-  m_leftFollowMotor.Follow(m_leftLeadMotor);
-  m_rightFollowMotor.Follow(m_rightLeadMotor);
+  //Allowing back motors to follow front motors/Shooter Gearbox
   m_shooter2.Follow(m_shooter1);
 
-
-  //Default
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 
   //Camera
   cs::UsbCamera camera = frc::CameraServer::StartAutomaticCapture();
@@ -40,52 +26,46 @@ void Robot::RobotInit() {
   //Encoders
   m_encoder1.Reset();
   m_encoder2.Reset();
-
   m_encoder1.SetDistancePerPulse((3.14159265358 * 6) / 360.0);
   m_encoder2.SetDistancePerPulse((3.14159265358 * 6) / 360.0);
+
 
   // Ultrasonic 
   ultrasonic_trigger_pin_one.Set(true);
 
-  //Colour Matcher
-  // m_colorMatcher.AddColorMatch(kBlueCargo);
-  // m_colorMatcher.AddColorMatch(kRedCargo);
-  // m_colorMatcher.AddColorMatch(kBlackLine); 
-  // m_colorMatcher.AddColorMatch(kBlueTarmac);
+
+  //Default
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 
 }
 
 void Robot::RobotPeriodic() {}
 
-void Robot::AutonomousInit() 
-{
+void Robot::AutonomousInit() {
+
 //Reset/Initializing Sensors
 m_encoder1.Reset();
 m_encoder2.Reset();
 ultrasonic_trigger_pin_one.Set(true);
 
-//Variables (bools, phases)
-
-// inRange = true;
-// once = true;
-// resetOnce=false;
-// encodersReset=false;
-// outtakeBallSecondTime=false;
+//Initializing Variables
 currentRed = 0.0;
 currentBlue = 0.0;
-distance = true;
 cargo_Outtake_Time = 0;
+cargo_Intake_Time = 0;
 phase=0;
-phase4=0;
 phase3=0;
+phase4=0;
 encoderAverage=0.0;
 reset=false;
-closetoCargo = false;
-increment = 0;
-SenseColour = false;
+
 }
 
+
 void Robot::AutonomousPeriodic() 
+
 {
 
 //Colour Sensor Periodic
@@ -96,232 +76,387 @@ frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
 currentRed = detectedColor.red;
 currentBlue = detectedColor.blue;
 
+
 //Ultrasonic Sensor Periodic
 double voltage_scale_factor = 5/frc::RobotController::GetVoltage5V();
 double  ultrasonic_sensor_range_one = ultrasonic_sensor_one.GetValue() * 0.0492 * voltage_scale_factor;
 frc::SmartDashboard::PutNumber("Sensor 1 Range", ultrasonic_sensor_range_one);
 frc::SmartDashboard::PutNumber("NAV sensor", (m_gyro.GetAngle()));
+
+
 //Encoder Periodic
 encoderAverage = (m_encoder1.GetDistance() + m_encoder2.GetDistance())/2;
 
+
+
 //********************************************************************************************************************************
+
 
 //ROUTE 1:
 
 // Phase 0: Drive from starting position to hub
+
 if (phase == 0) { 
-  if (ultrasonic_sensor_range_one > 20) { // Checking if ultrasonic is in range of minimum 12 inches away from hub
-    m_robotDrive.ArcadeDrive(0, 0.50); // Drive towards hub
+  
+  if (ultrasonic_sensor_range_one > 20) { //If farther than 20 inches from lower hub, driving until 20 inchs from lower hub
+    
+    m_robotDrive.ArcadeDrive(0, 0.50); 
     m_encoder1.Reset();
     m_encoder2.Reset();
+
   } 
-  else { // Ultrasonic is too close to be precise (12 or less away from hub)
-    // distance = false;
-    // if (encoderAverage > -6) { // Use encoders to measure distance now
-    //   m_robotDrive.ArcadeDrive(0, 0.50); // Drive until 6 inches from hub
-    // } 
-    phase = 1;
+
+  else { 
     
-  }
-}
-// Phase 1: Outtake ball
-if (phase == 1) {
-  if (cargo_Outtake_Time < 50) { // 50 multiplied by 20 millisecond period is approximately 1 second
-    // Outtake on
-    cargo_Outtake_Time = cargo_Outtake_Time + 1; 
-  }
-  else { // Done dropping cargo into hub
-    // Outtake off
-    phase = 2;
-    cargo_Outtake_Time = 0;
-    cargo_Intake_Time = 0;
+    phase = 1;
+
   }
 }
 
-// Phase 2: Drive to edge of tarmac
-if (phase == 2) {
-  if (currentRed < 0.286) {  // If current blue value is within 150 (this range can be tested) of past blue value, we are assuming that the surface being sensed has not changed
-    m_robotDrive.ArcadeDrive(0, -0.50); // Continue driving until tarmac outline reached; below we would align robot if we are doing that (maybe)
+// Phase 1: Outtake ball
+
+if (phase == 1) {
+
+  if (cargo_Outtake_Time < 50) { // 50 multiplied by 20 millisecond period is approximately 1 second, Used to rev up the outtake
+    
+    // Outtake on here
+    cargo_Outtake_Time = cargo_Outtake_Time + 1; 
+
+  }
+
+  else { //Outtake off, and onto the next phase
+    
+    phase = 2;
+    cargo_Outtake_Time = 0;
+    cargo_Intake_Time = 0;
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+
+  }
+
+}
+
+// Phase 2: Drive to edge of tarmac OR getting out of tarmac when reset happens
+
+if (phase == 2 && reset==false) {
+
+  if (currentRed < 0.286) {  // Driving until a high sense in colour, Depends on whether we are Red or Blue
+    
+    m_robotDrive.ArcadeDrive(0, -0.50); 
+
   } 
-  else { // Tarmac outlne reached (blue tape sensed) 
+  
+  else { //Reached Tarmac, onto phase 3
+    
     m_encoder1.Reset();
     m_encoder2.Reset();
     encoderAverage=0;
     phase = 3;
- // Prepare for phase 3
+
+  }
+
+}
+
+if (phase == 2 && reset==true) { //Exiting Tarmac
+
+  if (encoderAverage < 50) {
+
+    m_robotDrive.ArcadeDrive(0, -0.50);
+
+  }
+
+  else { //End of Route 1
+
+    phase = 4;
+
   }
 } 
 
 if (phase == 3) {
-  if (m_encoder1.GetDistance() < 15) { // Real distance = 350/8 inches
+  
+  if (m_encoder1.GetDistance() < 15) { //Real distance = 350/8 inches, Driving until we reach the cargo
+    
     m_robotDrive.ArcadeDrive(0, -0.50);
-    // if (encoderAverage < 10) {
-    //   closetoCargo=true;
-    // }
+ 
   }
-  // if (cargo_Intake_Time < 100) { 
-  //   // Intake on
-  //   cargo_Intake_Time++; 
-  //   m_robotDrive.ArcadeDrive(0, -0.50);
+  
+  //Intake comes here
+
   else if (reset==false) { // Done picking up cargo
-    // Intake off
+    
     phase = 0;
     reset = true;
     cargo_Intake_Time = 0;
     m_encoder1.Reset();
     m_encoder2.Reset(); // Back to phase 0
+
   }
+
   else {
+
     phase = 4;
+
   }
+
 }
 
 
- // Route 3
- if(phase3==0){//Outtake ball
-      if (cargo_Outtake_Time < 150) { 
-        // Outtake on
-        cargo_Outtake_Time++;
-      }else{
-        phase3++;
-      }
-  }
-  else if(phase3==1){//Go forwards til sense blue
-    if(currentBlue < 0.300) {
-      m_robotDrive.ArcadeDrive(0,-0.5);
-    } 
-    else{
-      phase3++;
-      m_encoder1.Reset();
-      m_encoder2.Reset();
-      m_gyro.Reset();
+//********************************************************************************************************************************
 
-    }
-  }
-  else if(phase3==2){//Turn X degrees
-    if(abs(m_gyro.GetAngle())<60){
-      m_robotDrive.ArcadeDrive(-0.6,0);
-    } else{
-      phase3++;
-      m_encoder1.Reset();
-      m_encoder2.Reset();
-    }
-  }
-  else if(phase3==3){//Go forwards 20 inches, and intake is on for last 10 inches 
-    if(m_encoder1.GetDistance()<=10){
-      m_robotDrive.ArcadeDrive(0,-0.5);
-    }else if(m_encoder1.GetDistance()<20){
-      //Turn on intake
-      m_robotDrive.ArcadeDrive(0,-0.5);
-    } else{
-      phase3++;
-      m_encoder1.Reset();
-      m_encoder2.Reset();
-    }
-  }
-  else if(phase3==4){//Go backwards till sense blue
-    if(currentBlue < 0.300) {
-      m_robotDrive.ArcadeDrive(0,0.5);
-    }else{
-      phase3++;
-      m_encoder1.Reset();
-      m_encoder2.Reset();
-      m_gyro.Reset();
-    }
-  }
-  else if(phase3==5){ // Turn X degrees
-    if(abs(m_gyro.GetAngle())<60){
-      m_robotDrive.ArcadeDrive(0.6,0);
-    } else{
-      phase3++;
-      m_encoder1.Reset();
-      m_encoder2.Reset();
-    }
-  }
-  else if(phase3==6){ //Go backwards until you are 12 inches from wall, using ultrasonic, ultrasonic bit buggy socomment out
-    if(ultrasonic_sensor_range_one>15){
-      m_robotDrive.ArcadeDrive(0,0.5);
-    } else{
-      phase3++;
-      m_encoder1.Reset();
-      m_encoder2.Reset();
-    }
-  }
-  else if(phase3==6){ //6 inches drive backwards to hub
-    if(m_encoder1.GetDistance()>-12){
-      m_robotDrive.ArcadeDrive(0,0.5);
-    } else{
-      phase3++;
-      m_encoder1.Reset();
-      m_encoder2.Reset();
-    }
-  }
-  else if(phase3==7){ //Outtake ball
-    if (cargo_Outtake_Time < 150) { 
-      // Outtake on
+
+// Route 3:
+
+if(phase3==0) { //Outtake ball
+    
+  if (cargo_Outtake_Time < 150) { 
+        
       cargo_Outtake_Time++;
-    }else{
-      phase3++;
+
     }
+    
+  else {
+        
+      phase3++;
+
+    }
+
+  }
+else if(phase3==1) {//Go forwards until we sense blue
+  
+  if(currentBlue < 0.286) {
+
+    m_robotDrive.ArcadeDrive(0,-0.5);
+
+  } 
+  
+  else {
+
+    phase3++;
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+    m_gyro.Reset();
+
   }
 
+}
 
-// Route 4
+else if(phase3==2) { //Turn 60 degrees
+  
+  if(abs(m_gyro.GetAngle())<60){
+
+    m_robotDrive.ArcadeDrive(-0.6,0);
+
+  } 
+  else{
+
+    phase3++;
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+
+  }
+
+}
+
+else if (phase3==3) {//Go forwards 20 inches, and intake is on for last 10 inches 
+  
+  if(m_encoder1.GetDistance()<=10){
+
+    m_robotDrive.ArcadeDrive(0,-0.5);
+
+  }
+  else if(m_encoder1.GetDistance()<20){
+
+    m_robotDrive.ArcadeDrive(0,-0.5);
+
+  } 
+  else{
+
+    phase3++;
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+
+  }
+
+}
+
+else if(phase3==4){//Go backwards till sense blue
+  
+  if(currentBlue < 0.300) {
+
+    m_robotDrive.ArcadeDrive(0,0.5);
+
+  }
+  else{
+
+    phase3++;
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+    m_gyro.Reset();
+
+  }
+
+}
+
+else if (phase3==5) { // Turn X degrees
+  
+  if(abs(m_gyro.GetAngle())<60){
+
+    m_robotDrive.ArcadeDrive(0.6,0);
+
+  } 
+  else{
+
+    phase3++;
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+
+  }
+
+}
+
+else if (phase3==6) { //Go backwards until you are 12 inches from wall, using ultrasonic, ultrasonic bit buggy socomment out
+  
+  if(ultrasonic_sensor_range_one>15){
+
+    m_robotDrive.ArcadeDrive(0,0.5);
+
+  } 
+  else{
+
+    phase3++;
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+
+  }
+
+}
+
+else if (phase3==6) { //6 inches drive backwards to hub
+  
+  if(m_encoder1.GetDistance()>-12){
+
+    m_robotDrive.ArcadeDrive(0,0.5);
+
+  } 
+  else{
+
+    phase3++;
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+
+  }
+
+}
+
+else if (phase3==7) { //Outtake ball
+  if (cargo_Outtake_Time < 150) { 
+
+    cargo_Outtake_Time++;
+
+  }
+  else {
+
+    phase3++;
+
+  }
+
+}
+
+
+//********************************************************************************************************************************
+
+
+// Route 4:
 
 if (phase4==0){ //Turn 180 degrees
-    if(m_gyro.GetAngle()>-180){
-        m_robotDrive.ArcadeDrive(0.6,0);
-      }
-      else{
-        phase4=1;
-        m_encoder1.Reset();
-        m_encoder2.Reset();
-      }
-    }
-    if (phase4==1) //Go back until 40 inches away from wall
-    {
-      if (ultrasonic_sensor_range_one>=20.0){
-        m_robotDrive.ArcadeDrive(0, 0.5); // Drive until 40 inches from back wall
-        m_encoder1.Reset();
-        m_encoder2.Reset();
-      }else{
-        phase4=2;
-       m_gyro.Reset();
 
-      }
+  if(m_gyro.GetAngle()>-180){
+
+    m_robotDrive.ArcadeDrive(0.6,0);
+
+  }
+  
+  else {
+
+    phase4=1;
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+
+  }
+
+}
+
+if (phase4==1) { //Go back until 40 inches away from wall
+
+  if (ultrasonic_sensor_range_one>=20.0) {
+
+    m_robotDrive.ArcadeDrive(0, 0.5); // Drive until 40 inches from back wall
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+
+  }
+  
+  else {
+
+    phase4=2;
+    m_gyro.Reset();
+
+  }
+
+}
+
+if (phase4==2){ //Turn 90 degrees
+  
+  if(abs(m_gyro.GetAngle())<90) {
+
+    m_robotDrive.ArcadeDrive(0.6,0);
+
+  }
+  
+  else {
+
+    phase4=3;
+    m_encoder1.Reset();
+    m_encoder2.Reset();
+
+  }
+
+}
+
+if (phase4==3) { //Go fowards till terminal ball and push it to the human player
+  
+  if (m_encoder2.GetDistance()>=-20) {
+
+    m_robotDrive.ArcadeDrive(0,0.5);
+
+  } 
+  
+  else{
+
+    if (cargo_Intake_Time < 150) { 
+
+      cargo_Intake_Time++; 
+      m_robotDrive.ArcadeDrive(0, 0.5);
+      phase4=4;
+
     }
-  if (phase4==2){ //Turn 90 degrees
-      if(abs(m_gyro.GetAngle())<90){
-        m_robotDrive.ArcadeDrive(0.6,0);
-      }else{
-        phase4=3;
-        m_encoder1.Reset();
-        m_encoder2.Reset();
-      }
-    }
-    //Go fowards till terminal ball and push it to the human player
-    if (phase4==3){
-      if(m_encoder2.GetDistance()>=-20){
-        m_robotDrive.ArcadeDrive(0,0.5);
-      } else{
-        if (cargo_Intake_Time < 150) { 
-          // Intake on
-          cargo_Intake_Time++; 
-          m_robotDrive.ArcadeDrive(0, 0.5);
-          phase4=4;
-        }
-      }
-    //That is it???
-    }
+
+  }
+
+}
+
 } 
 
 void Robot::TeleopInit() {
+
   m_encoder1.Reset();
   m_encoder2.Reset();
   m_gyro.Reset();
+
 }
 
 void Robot::TeleopPeriodic() {
+
   // IntakeMovement();
   // IntakeOnAndOff();
   Storage();
@@ -331,42 +466,52 @@ void Robot::TeleopPeriodic() {
   // Hanging2();
   // Hanging3();
   SmartDashboard();
+
 }
 
 void Robot::Movement() {
+
   // Drive with arcade style
-  float xDrive = m_xbox.GetX() * -0.8;
-  float yDrive = m_xbox.GetY() * 0.8;
+  float xDrive = m_xbox.GetX() * 0.8;
+  float yDrive = m_xbox.GetY() * -0.8;
   m_robotDrive.ArcadeDrive(xDrive, yDrive);
+
 }
 
 void Robot::SmartDashboard() {
+
   // Encoder SmartDashboard
   frc::SmartDashboard::PutNumber("Encoder 1 Distance: ", m_encoder1.GetDistance());
   frc::SmartDashboard::PutNumber("Encoder 2 Distance: ", m_encoder2.GetDistance());
-  frc::SmartDashboard::PutNumber("NAV sensor", abs(m_gyro.GetAngle()));
   frc::SmartDashboard::PutNumber("Robot Displacement: ", (m_encoder1.GetDistance() + m_encoder2.GetDistance())/2);
+
+  //Gyro SmartDashboard
+  frc::SmartDashboard::PutNumber("NAV sensor", abs(m_gyro.GetAngle()));
+
+  //Colour Sensor SmartDashboard
   frc::Color detectedColor = m_colorSensor.GetColor();  
   frc::SmartDashboard::PutNumber("Red", detectedColor.red);
   frc::SmartDashboard::PutNumber("Green", detectedColor.green);
   frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
+
+  //Ultrasonic SmartDashboard
   double voltage_scale_factor = 5/frc::RobotController::GetVoltage5V();
   double  ultrasonic_sensor_range_one = ultrasonic_sensor_one.GetValue() * 0.0492 * voltage_scale_factor;
-  double  ultrasonic_sensor_range_two = ultrasonic_sensor_one.GetValue() * 0.0492 * voltage_scale_factor;
   frc::SmartDashboard::PutNumber("Sensor 1 Range", ultrasonic_sensor_range_one);
-  frc::SmartDashboard::PutNumber("Sensor 2 Range", ultrasonic_sensor_range_two);
-  // frc::SmartDashboard::PutData("Field", &m_field);
+
 }
 
 void Robot::Storage() {
+
   m_storage.Set(m_xbox.GetRawAxis(3));
+
 }
 
 void Robot::Outake() {
+
   m_shooter1.Set(m_xbox.GetRawAxis(1));
+
 }
-
-
 
 void Robot::DisabledInit() {}
 
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+//Libraries
 #include "AHRS.h"
 #include <string>
 #include <frc/Joystick.h>
@@ -12,7 +13,6 @@
 #include <frc/drive/DifferentialDrive.h>
 #include "rev/CANSparkMax.h"
 #include <frc/smartdashboard/SmartDashboard.h>
-#include <frc/smartdashboard/Field2d.h>
 #include <frc/Encoder.h>
 #include <frc/simulation/EncoderSim.h>
 #include <frc/AnalogInput.h>
@@ -28,13 +28,12 @@
 #include <frc/SpeedControllerGroup.h>
 #include "ctre/Phoenix.h"
 
+
 class Robot : public frc::TimedRobot {
 
 public:
-  
-  //Gyroscope
-  AHRS m_gyro{frc::SPI::Port::kMXP};
 
+  //Functions
   void RobotInit() override;
   void RobotPeriodic() override;
   void AutonomousInit() override;
@@ -45,18 +44,24 @@ public:
   void DisabledPeriodic() override;
   void TestInit() override;
   void TestPeriodic() override;
+  void IntakeMovement();
+  void IntakeOnAndOff();
+  void Storage();
+  void Outake();
+  void Movement();
+  void Hanging1();
+  void Hanging2();
+  void Hanging3();
+  void SmartDashboard();
 
 private:
 
   //CAN Pin Constants
   static const int leftLeadDeviceID = 0;
-  // static const int leftFollowDeviceID = 3;
   static const int rightLeadDeviceID = 1;
-  // static const int rightFollowDeviceID = 4;
-  static const int Shooter1RioPin = 1;
-  static const int Shooter2RioPin = 2;
-  static const int IntakeMotorRioPin = 3;
-
+  static const int shooter1ID = 5;
+  static const int shooter2ID = 6;
+  static const int storageID = 7;
 
 
   //Encoder Pin Constants
@@ -64,9 +69,6 @@ private:
   static const int EncoderPin1B = 1;
   static const int EncoderPin2A = 2;
   static const int EncoderPin2B = 3;
-
-  // Field2d
-  // frc::Field2d m_field;
 
   // I2C Port
   static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
@@ -76,36 +78,23 @@ private:
   rev::ColorMatch m_colorMatcher;
   float currentRed;
   float currentBlue;
-
   
-
-  // RGB Values
-  // static constexpr frc::Color kBlueCargo = frc::Color(0, 0, 0);
-  // static constexpr frc::Color kRedCargo = frc::Color(0, 0, 0);
-  // static constexpr frc::Color kBlueTarmac = frc::Color(0, 0, 0);
-  // static constexpr frc::Color kBlackLine = frc::Color(0, 0, 0); //We'll need to detect these on Sunday
-
-  // bool arrivedDestination = false;
+  //Gyro
+  AHRS m_gyro{frc::SPI::Port::kMXP};
 
   //Joystick
   frc::Joystick m_xbox{ 0 };
 
-  //Drive
+  //DifferentialDrive
   WPI_VictorSPX frontLeft = {leftLeadDeviceID};
   WPI_VictorSPX frontRight = {rightLeadDeviceID};
-  // rev::CANSparkMax m_leftLeadMotor{leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushed};
-  // rev::CANSparkMax m_rightLeadMotor{rightLeadDeviceID, rev::CANSparkMax::MotorType::kBrushed};
-  // rev::CANSparkMax m_leftFollowMotor{leftFollowDeviceID, rev::CANSparkMax::MotorType::kBrushed};
-  // rev::CANSparkMax m_rightFollowMotor{rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushed};
   frc::DifferentialDrive m_robotDrive{frontLeft, frontRight};
 
-//minecraft
-
   //Intake/Shooter
-  frc::PWMVictorSPX m_Shooter1Motor {Shooter1RioPin};
-  frc::PWMVictorSPX m_Shooter2Motor {Shooter2RioPin};
-  frc::PWMVictorSPX m_IntakeMotor {IntakeMotorRioPin};
-  frc::SpeedControllerGroup m_Shooter {m_Shooter1Motor, m_Shooter2Motor};
+  rev::CANSparkMax m_shooter1{shooter1ID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_shooter2{shooter1ID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_storage{storageID, rev::CANSparkMax::MotorType::kBrushless};
+
   //Encoder Set Up
   frc::Encoder m_encoder1{ EncoderPin1A, EncoderPin1B, true };
   frc::Encoder m_encoder2{ EncoderPin2A, EncoderPin2B, false };
@@ -114,27 +103,16 @@ private:
   // Ultrasonic Set Up
   frc::AnalogInput ultrasonic_sensor_one{0};
   frc::DigitalOutput ultrasonic_trigger_pin_one{4};
-  double ultrasonic_sensor_range_one = 0.0;
-  
-  // frc::AnalogInput ultrasonic_sensor_two{0};
-  // frc::DigitalOutput ultrasonic_trigger_pin_two{4};
-  // double ultrasonic_sensor_range_two = 0.0;
-
+  double ultrasonic_sensor_range_one = 0.0; 
   double voltage_scale_factor = 1.0;
 
-  // Autonomous Set Up
-  int counter;
+  // Autonomous Variables
   int phase;
   int phase4;
   int phase3;
   int cargo_Outtake_Time;
   int cargo_Intake_Time;
-  bool distance;
   bool reset;
-  bool closetoCargo;  
-  int increment;
-  bool SenseColour;
-  // bool once;
 
   //Default
   frc::SendableChooser<std::string> m_chooser;
